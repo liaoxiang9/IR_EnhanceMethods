6e94931be430065ed40f50b5cbebb872b0a8bde1
liaoxiang9 <lx1500692043@aliyun.com>
2022-11-17 11:44:44 +0800

    test

--- IR_denoise/src/main.cpp
+++ IR_denoise/src/main.cpp
@@ -5,7 +5,7 @@ using namespace cv;
 
 int main(){
     NLM n1(11, 21, 10);
-    cv::Mat img_noise = cv::imread("/home/lx/IR_Enhance_Methods/src_images/1.jpg", cv::IMREAD_GRAYSCALE);
+    cv::Mat img_noise = cv::imread("/home/lx/IR_EnhanceMethods/src_images/1.jpg", cv::IMREAD_GRAYSCALE);
     // 检测图像是否读取成功
     if(img_noise.empty()){
         std::cout << "Error: Image not found!" << std::endl;
@@ -18,9 +18,9 @@ int main(){
     cv::randn(noise, 0, 10);
     cv::add(img_noise, noise, img_noise_gaussian);
     // 存图
-    cv::imwrite("/home/lx/IR_Enhance_Methods/src_images/noise_gaussian.jpg", img_noise_gaussian);
+    cv::imwrite("/home/lx/IR_EnhanceMethods/src_images/noise_gaussian.jpg", img_noise_gaussian);
 
-    n1.set_image("/home/lx/IR_Enhance_Methods/src_images/noise_gaussian.jpg");
+    n1.set_image("/home/lx/IR_EnhanceMethods/src_images/noise_gaussian.jpg");
     cv::Mat dst;
     n1.denoise(dst);
     // cv::imwrite("../result/dst.jpg", dst);
